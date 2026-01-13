9ec230ae0ef7ccd4b6f100b1ede8c725d07763f8
jlaplaine31 <jeremy@planel.net>
2026-01-13 11:30:36 +0100

    Revert dialog size to 80%

--- js/taskpane.js
+++ js/taskpane.js
@@ -56,7 +56,7 @@ function openFullScreenApp() {
 
     Office.context.ui.displayDialogAsync(
         dialogUrl,
-        { height: 100, width: 100 },
+        { height: 80, width: 80 },
         (result) => {
             if (result.status === Office.AsyncResultStatus.Succeeded) {
                 dialog = result.value;
